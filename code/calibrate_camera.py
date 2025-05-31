8d2c7cef0f549edcca8ad59cb667356cbd799a79
Quentin Bolsee <quentinbolsee@hotmail.com>
2025-05-31 16:07:23 -0400

    start code

---
+++ code/calibrate_camera.py
@@ -0,0 +1,19 @@
+import datetime
+import os
+import sys
+import argparse
+import math
+
+import cv2
+import numpy as np
+
+
+def main():
+    filename = "data/grid.png"
+
+    img = cv2.imread(filename)
+
+
+
+if __name__ == "__main__":
+    main()
