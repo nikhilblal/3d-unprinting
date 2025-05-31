b9a6e450ad08a172fa5164764c67714868ef4823
Quentin Bolsee <quentinbolsee@hotmail.com>
2025-05-31 16:18:46 -0400

    code progress

--- code/calibrate_camera.py
+++ code/calibrate_camera.py
@@ -3,16 +3,32 @@ import os
 import sys
 import argparse
 import math
+import json
 
 import cv2
 import numpy as np
 
 
 def main():
-    filename = "data/grid.png"
+    filename_grid = "data/grid.png"
+    filename_config = "config.json"
 
-    img = cv2.imread(filename)
+    img = cv2.imread(filename_grid)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    with open(filename_config, "r") as f:
+        config = json.load(f)
+
+
+    m = config["grid"]["rows"]
+    n = config["grid"]["columns"]
+    s = config["grid"]["size_mm"]
+
+    ret, corners = cv2.findChessboardCorners(img_gray, (m, n), None)
+
+    # img = cv2.drawChessboardCorners(img, (m, n), corners)
+    # print(corners)
+    # print(ret)
 
 
 if __name__ == "__main__":
